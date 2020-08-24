8ce0aca8d8e18ceee6bdb94a2e3a654a34b0df31
Matthew Cucco <mcucco@thermatru.com>
2020-08-24 15:47:44 -0400

    Added projects and Alpaca

---
+++ stocktrader.Models/Stock.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace stocktrader.Models
+{
+    public class Stock
+    {
+    }
+}
---
+++ stocktrader.Services/TradingService.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace stocktrader.Services
+{
+    public class TradingService
+    {
+    }
+}
