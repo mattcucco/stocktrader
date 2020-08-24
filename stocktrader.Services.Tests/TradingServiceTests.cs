2e974b3918d8f9d751441681712b349b24c3084a
Matthew Cucco <mcucco@thermatru.com>
2020-08-24 16:12:57 -0400

    Added alpaca trading & data clients

---
+++ stocktrader.Services.Tests/TradingServiceTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace stocktrader.Services.Tests
+{
+    [TestClass]
+    public class TradingServiceTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
--- stocktrader.Services/TradingService.cs
+++ stocktrader.Services/TradingService.cs
@@ -1,8 +1,53 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Alpaca.Markets;
 
 namespace stocktrader.Services
 {
-    public class TradingService
+    public class TradingService : IDisposable
     {
+        #region <|| PROPERTIES ||>
+        private AlpacaTradingClient TradingClient { get; set; }
+        private AlpacaDataClient DataClient { get; set; }
+
+        #endregion <|| PROPERTIES ||>
+
+        #region <|| CONSTRUCTORS ||>
+        public TradingService(string key, string value)
+        {
+            TradingClient = Alpaca.Markets.Environments.Paper.GetAlpacaTradingClient(new SecretKey(key, value));
+            DataClient = Alpaca.Markets.Environments.Paper.GetAlpacaDataClient(new SecretKey(key, value));
+        }
+
+        public void Dispose()
+        {
+            TradingClient?.Dispose();
+            DataClient?.Dispose();
+        }
+
+        #endregion <|| CONSTRUCTORS ||>
+
+        #region <|| PUBLIC METHODS ||>
+        public async Task<bool> IsMarketOpen()
+        {
+            var clock = await TradingClient.GetClockAsync();
+            return clock.IsOpen;
+        }
+        public async Task<DateTime> GetNextMarketOpen()
+        {
+            var clock = await TradingClient.GetClockAsync();
+            return clock.NextOpen;
+        }
+        public async Task<DateTime> GetNextMarketClose()
+        {
+            var clock = await TradingClient.GetClockAsync();
+            return clock.NextClose;
+        }
+        #endregion <|| PUBLIC METHODS ||>
+
+        #region <|| PRIVATE METHODS ||>
+
+
+        #endregion <|| PRIVATE METHODS ||>
     }
 }
